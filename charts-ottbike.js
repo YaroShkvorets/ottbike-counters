e6af4f77175dcaef34bba30279c484b74b04696b
zzptichka <shkvorets@gmail.com>
2018-05-11 21:18:06 -0400

    code cleanup

--- charts-ottbike.js
+++ charts-ottbike.js
@@ -6,13 +6,12 @@ const optionsYears = {
 				zoomType: 'xy'
 			},
 	title: {
-		text: 'Laurier Bike Lane'
+		text: 'Bike Lane'
 	},
 	legend: {
           enabled: false
       },
 	xAxis: {
-	   //type: 'datetime',
 	   gridLineWidth: 1,
 	   tickPositions: [0,31,59,90,120,151,181,212,243,273,304,334,365],
 	   labels: {
@@ -37,31 +36,22 @@ const optionsYears = {
 
 	},
 	tooltip: {
-          //crosshairs: true,
-          shared: true,
+    shared: true,
 		useHTML: true,
 		headerFormat: '<span style="font-size:10px">Day {point.x}</span>',
 		formatter: function () {
               return TooltipFormatter(this, false);
           }
-
       },
-
 	series: []
 };
 
 const optionsMonths = {
-
 	chart: {
 		type: 'column',
 		renderTo: 'container_months',
-		zoomType: 'xy'
-
 	},
 	title: {
-		text: null
-	},
-	subtitle: {
 		text: null
 	},
 	xAxis: {
@@ -108,9 +98,7 @@ const optionsDays = {
 
 	chart: {
 		type: 'column',
-		renderTo: 'container_days',
-		zoomType: 'xy'
-
+		renderTo: 'container_days'
 	},
 	legend: {
           enabled: false
@@ -118,12 +106,8 @@ const optionsDays = {
 	title: {
 		text: null
 	},
-	subtitle: {
-		text: null
-	},
 	xAxis: {
 		type: 'datetime',
-		//tickInterval: 7 * 24 * 3600 * 1000,
 		plotBands: []
 
 	},
@@ -153,17 +137,13 @@ const optionsWeeks = {
 
 	chart: {
 		type: 'column',
-		renderTo: 'container_weeks',
-		zoomType: 'xy'
+		renderTo: 'container_weeks'
 
 	},
 	legend: {
           enabled: false
       },
 	title: {
-		text: null
-	},
-	subtitle: {
 		text: null
 	},
 	xAxis: {
@@ -205,7 +185,7 @@ const WeeksSeries = {
             name: 'Week total',
             data: []
         };
-var thisWeekSeries = {
+const ThisWeekSeries = {
             name: 'This week total',
             color: '#ffa126',
             data: []
@@ -216,92 +196,73 @@ let MonthWeekRecords = []
 let Years = []
 let todayTotals = [];
 
+const now = new Date();
+
+
 function getSortedPointsKeys(points) {
-    var keys = []; for(var key in points) keys.push(key);
+    const keys = []; for(let key in points) keys.push(key);
     return keys.sort(function(a,b){return points[b].y-points[a].y});
 }
 
 function getSortedKeys(obj) {
-    var keys = []; for(var key in obj) keys.push(key);
+    const keys = []; for(let key in obj) keys.push(key);
     return keys.sort(function(a,b){return obj[b]-obj[a]});
 }
 
 
 function TooltipFormatter(tooltip) {
 
-	var sortedPoints = getSortedPointsKeys(tooltip.points);
+	const sortedPoints = getSortedPointsKeys(tooltip.points);
 
-    // Create the header with reference to the time interval
-    var index = tooltip.points[0].point.index,
-        ret = '<small>' + GetDayMonthFromDayNum(tooltip.x) + '</small><br>';
+  // Create the header with reference to the time interval
+  const index = tooltip.points[0].point.index;
+  let ret = '<small>' + GetDayMonthFromDayNum(tooltip.x) + '</small><br><table>';
 
-
-    ret += '<table>';
-
-	for(var key in sortedPoints)
+	for(let key in sortedPoints)
 	{
-        var series = tooltip.points[sortedPoints[key]].series;
-		var yearShow = tooltip.points[sortedPoints[key]].y;		//whichever year we are showing
-		var yearCur = tooltip.points[tooltip.points.length-1].y;		//2017
-		var diff = '<span style="font-size: 75%;">' + (yearCur>yearShow?' (':' (+');
+    const series = tooltip.points[sortedPoints[key]].series;
+		const yearShow = tooltip.points[sortedPoints[key]].y;		//whichever year we are showing
+		const yearCur = tooltip.points[tooltip.points.length-1].y;		//2017
+		let diff = '<span style="font-size: 75%;">' + (yearCur>yearShow?' (':' (+');
 		diff+=Highcharts.numberFormat(yearShow-yearCur,0)+') </span>';
 		if(yearShow==yearCur) diff='';
 
 		ret += '<tr><td><span style="color:' + series.color + ';">\u25CF</span> ' + series.name +
 			': </td><td style="margin:0;white-space:nowrap"><b>' + Highcharts.numberFormat(yearShow,0) +
 			'</b></td><td>'+ diff +'</td></tr>';
+  }
 
-    }
+  ret += '</table>';
 
-    // Close
-    ret += '</table>';
-
-
-    return ret;
+  return ret;
 };
 
 
 function GetDayMonthFromDayNum(daynum)
 {
-	var date = new Date(2018, 0); // initialize a date in `year-01-01`
-	var date1 = new Date(date.setDate(daynum+1)); // add the number of days
-
-	return Highcharts.dateFormat('%b %e', date1.getTime())
-
-	/*return date1.toLocaleDateString('en-GB', {
-		day : 'numeric',
-		month : 'short',
-		year : 'numeric'
-	}).split(' ');*/
+	const date = new Date(new Date(2018, 0).setDate(daynum+1)); // add the number of days
+	return Highcharts.dateFormat('%b %e', date.getTime())
 };
 
 function GetMonthFromDayNum(daynum)
 {
-	var date = new Date(2018, 0); // initialize a date in `year-01-01`
-	var date1 = new Date(date.setDate(daynum+1)); // add the number of days
-
-	return Highcharts.dateFormat('%b', date1.getTime())
-
-	/*return date1.toLocaleDateString('en-GB', {
-		day : 'numeric',
-		month : 'short',
-		year : 'numeric'
-	}).split(' ');*/
+	const date = new Date(new Date(2018, 0).setDate(daynum+1)); // add the number of days
+	return Highcharts.dateFormat('%b', date.getTime())
 };
 
 
 function renderYearsTextBox(todayTotals)
 {
 	const curYear = todayTotals.length-1
-	var sortedYears = getSortedKeys(todayTotals);
+	const sortedYears = getSortedKeys(todayTotals);
 
-	var labelText = 'To this day:<br>';
+	let labelText = 'To this day:<br>';
 
-	for(var key in sortedYears)
+	for(let key in sortedYears)
 	{
-		var year = sortedYears[key];
-		var series = chartYears.get(parseInt(year));//YearsSeries[sortedYears[key]];
-		var diff = '<span style="font-size: 75%;">' + (todayTotals[curYear]>todayTotals[sortedYears[key]]?' (':' (+');
+		const year = sortedYears[key];
+		const series = chartYears.get(parseInt(year));
+		let diff = '<span style="font-size: 75%;">' + (todayTotals[curYear]>todayTotals[sortedYears[key]]?' (':' (+');
 		diff+=Highcharts.numberFormat(todayTotals[sortedYears[key]]-todayTotals[curYear],0)+') </span>';
 		if(curYear==sortedYears[key]) diff='';
 
@@ -310,6 +271,7 @@ function renderYearsTextBox(todayTotals)
 
 	}
 
+	//now draw legend box in the top left corner
 	chartYears.renderer.text(labelText, 80, 80)
 		.attr({
 			zIndex: 5
@@ -327,8 +289,8 @@ function AddRecordsLines()
 
   const lastDayTime = DaysSeries.data[DaysSeries.data.length-1][0];
 
-	var monthOfYear = new Date(lastDayTime+24*60*60*1000).getMonth()+1;	//check one day into future because timezones
-	var dayOfWeek = new Date(lastDayTime).getDay();
+	const monthOfYear = new Date(lastDayTime+24*60*60*1000).getMonth()+1;	//check one day into future because timezones
+	const dayOfWeek = new Date(lastDayTime).getDay();
 	const MonthRecord = MonthDayRecords[monthOfYear];
 	const DayOfWeekRecord = DayOfWeekRecords[dayOfWeek];
 	const MonthWeekRecord = MonthWeekRecords[monthOfYear];
@@ -390,55 +352,51 @@ function AddRecordsLines()
 
 };
 
-const now = new Date();
-
 
 
 $(document).ready(function(){
 
-  $('#laurier').click(function ()
-  {
-    chartDays.destroy();
-    chartMonths.destroy();
-    chartYears.destroy();
-    chartWeeks.destroy();
-		optionsYears.title.text = 'Laurier Bike Lane'
-    showCounter('laurier.csv')
+	jQuery.ajaxSetup({async:false});
+
+	$('#laurier').click(function (){
+    showCounter('Laurier Bike Lane', 'laurier.csv')
   });
 
-  $('#portage').click(function ()
-  {
-    chartDays.destroy();
-    chartMonths.destroy();
-    chartYears.destroy();
-    chartWeeks.destroy();
-		optionsYears.title.text = 'Portage Bike Lane'
-    showCounter('portage.csv')
+  $('#portage').click(function (){
+		showCounter('Portage Bike Lane', 'portage.csv')
   });
 
-  jQuery.ajaxSetup({async:false});
-  showCounter('laurier.csv')
+  showCounter('Laurier Bike Lane', 'laurier.csv')
 
 })
 
-function showCounter(filename){
+function destroyCharts(){
+	if (typeof chartDays != 'undefined') chartDays.destroy();
+	if (typeof chartMonths != 'undefined') chartMonths.destroy();
+	if (typeof chartYears != 'undefined') chartYears.destroy();
+	if (typeof chartWeeks != 'undefined') chartWeeks.destroy();
 
-  Years = []
-  DaysSeries.data=[]
-  WeeksSeries.data=[]
-	thisWeekSeries.data=[]
-  MonthDayRecords = []
-  DayOfWeekRecords = []
-  MonthWeekRecords = []
-  todayTotals = []
-  optionsYears.series = []
-  optionsMonths.series = []
-  optionsDays.series = []
-  optionsWeeks.series = []
-  optionsDays.yAxis.plotLines = []
-  optionsWeeks.yAxis.plotLines = []
+	Years = []
+	DaysSeries.data=[]
+	WeeksSeries.data=[]
+	ThisWeekSeries.data=[]
+	MonthDayRecords = []
+	DayOfWeekRecords = []
+	MonthWeekRecords = []
+	todayTotals = []
+	optionsYears.series = []
+	optionsMonths.series = []
+	optionsDays.series = []
+	optionsWeeks.series = []
+	optionsDays.yAxis.plotLines = []
+	optionsWeeks.yAxis.plotLines = []
+}
 
+function showCounter(name, filename){
+
+	destroyCharts()
   parseBikeData(filename, Years)
+
   const todayPoint = Years[Years.length-1].daily.length-1;
 
   for(let year of Years){
@@ -447,10 +405,11 @@ function showCounter(filename){
       todayTotals[year.year] = year.daily[todayPoint];
   }
   optionsDays.series.push(DaysSeries);
-  thisWeekSeries.data.push(WeeksSeries.data[WeeksSeries.data.length-1]);
+  ThisWeekSeries.data.push(WeeksSeries.data[WeeksSeries.data.length-1]);
   WeeksSeries.data.length = WeeksSeries.data.length-1;
   optionsWeeks.series.push(WeeksSeries);
-  optionsWeeks.series.push(thisWeekSeries);
+  optionsWeeks.series.push(ThisWeekSeries);
+	optionsYears.title.text = name
 
   AddRecordsLines();
 
@@ -462,11 +421,11 @@ function showCounter(filename){
 }
 
 function parseBikeData(pathToFile, Years){
-  let curMonth = 12;
-	let	curYear = -1;
-	let	curMonthRides = 0;
-	let	curWeekRides = 0;
-  let weekStart = 0;
+  let curMonth = 12,
+		curYear = -1,
+		curMonthRides = 0,
+		curWeekRides = 0,
+		weekStart = 0
 
 
   $.get( pathToFile, function( data ) {  //go through lines
@@ -474,21 +433,21 @@ function parseBikeData(pathToFile, Years){
     let objYear = {}
 
     $.each(lines, function(lineNo, line) {  //go through comma-separated values in line
-      var items = line.split(',');
-      if(line=='') {
-        objYear.monthly.push(curMonthRides);
-        Years.push(objYear)
-				if(curWeekRides != 0)	WeeksSeries.data.push([weekStart,curWeekRides])
-        return
+      if(line=='') {	//last line - wrap up and return
+        if(curMonthRides != 0) {
+					objYear.monthly.push(curMonthRides);
+        	Years.push(objYear)
+				}
+				if(curWeekRides != 0)	{
+					WeeksSeries.data.push([weekStart,curWeekRides])
+        }
+				return
       }
-      const values = line.split(',');
+			const items = line.split(',')
 
-      const array = values[0].split('-')
-      const year = parseInt(array[0])
-      const month = parseInt(array[1])
-      const day = parseInt(array[2])
+			const [year,month,day] = items[0].split('-').map(function(ele){return parseInt(ele)})
 
-      if(year != curYear){
+      if(year != curYear){		//new year!
         if(curYear!=-1){
           Years.push(objYear)
           objYear.monthly.push(curMonthRides);
@@ -498,8 +457,7 @@ function parseBikeData(pathToFile, Years){
         curYear = year
       }
 
-      if(curMonth!=month)
-      {
+      if(curMonth!=month)	{	//new month!
         if(curMonth<month){
           objYear.monthly.push(curMonthRides);
           curMonthRides = 0;
@@ -507,19 +465,18 @@ function parseBikeData(pathToFile, Years){
         curMonth=month;
       }
 
-      var date = items[0];
-      var rides = parseFloat(items[1]);
+      const date = items[0],
+   			rides = parseInt(items[1]),
+				time = Date.parse(date),
+				nowtime = now.getTime(),
+				dayOfWeek = new Date(time).getDay()
 
-      var time = Date.parse(date);
-      var nowtime = now.getTime();
-      var dayOfWeek = new Date(time).getDay();
+      if(dayOfWeek == 0){	//Monday..Sunday = week
 
-      if(dayOfWeek == 0)	//Monday..Sunday = week
-      {
         if(weekStart!=0){
           WeeksSeries.data.push([weekStart,curWeekRides])
-          if(typeof(MonthWeekRecords[curMonth])== 'undefined' || MonthWeekRecords[curMonth].rides<WeeksSeries.data[WeeksSeries.data.length-1][1])
-          {
+          if(typeof(MonthWeekRecords[curMonth])== 'undefined' || MonthWeekRecords[curMonth].rides<WeeksSeries.data[WeeksSeries.data.length-1][1]){
+
             MonthWeekRecords[curMonth] = {rides:WeeksSeries.data[WeeksSeries.data.length-1][1], time: WeeksSeries.data[WeeksSeries.data.length-1][0]}
           }
         }
@@ -535,25 +492,21 @@ function parseBikeData(pathToFile, Years){
         DayOfWeekRecords[dayOfWeek] = {time:time, rides:rides};
       }
 
-      if(time > nowtime - 90*24*60*60*1000)	//last 90 days
-      {
-        var d = new Date(time);
+      if(time > nowtime - 90*24*60*60*1000) {	//Days chart for last 90 days
+
         DaysSeries.data.push([time,rides]);
-        if(d.getDay()==4){
+        if(new Date(time).getDay()==4){	//weekends - yellow stripes
           optionsDays.xAxis.plotBands.push({color: '#FCFFC5', from: time+12*60*60*1000, to: time+2.5*24*60*60*1000});
         }
-
       }
 
       if(month==2 && day==29)
-        return true;		//continue
+        return;		//skip leap day for uniformity
 
       objYear.ridesTotal+=rides;
 
       if(objYear.daily.length<365) 				//sometimes we have double-days that skew axis
         objYear.daily.push(objYear.ridesTotal);
-
-
 
       curMonthRides+=rides;
 
